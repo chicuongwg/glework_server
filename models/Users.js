0f6c1780e02ef4d27a422d787fff765b0d64488c
lebadac <21521911@gm.uit.edu.vn>
2024-10-21 01:16:59 +0700

    Done: User registration, email confirmation, password reset & MySQL connection

---
+++ models/Users.js
@@ -0,0 +1,37 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define("User", {
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        dateOfBirth: {
+            type: DataTypes.DATEONLY,
+            allowNull: false,
+        },
+        phoneNumber: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+            isEmail: true, // Validate email format
+            },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        isConfirmed: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false, // Mặc định là chưa xác nhận
+        },
+    });
+
+    return User;
+};
