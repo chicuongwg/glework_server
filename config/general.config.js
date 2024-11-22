1ab498379bef1d98abd18e2bbffe9ea5c9a0a16d
lebadac <21521911@gm.uit.edu.vn>
2024-11-22 17:54:17 +0700

    feat: Add JWT authentication for user sessions

--- config/general.config.js
+++ config/general.config.js
@@ -10,5 +10,9 @@ module.exports = {
     email: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
-    },    
+    },
+    jwt: {
+        secret: 'abxyz',
+        tokenLoginExpiredDays: '1d'
+    },
 };
